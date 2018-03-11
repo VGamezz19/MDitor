03e14d201356a52b976c3d159644cc6805ff265b
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-11 18:20:14 +0100

    React-Materialize

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Button, Icon } from 'react-materialize'
 
 class App extends Component {
   render() {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button waves='light'>
+          <Icon>thumb_up</Icon>
+        </Button>
       </div>
     );
   }
