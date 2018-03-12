4199ec5369ca64abda7aee845c0382f5848a5524
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-12 10:00:50 +0100

    baby-sidenav

--- src/components/Sidenav/Sidenav.js
+++ src/components/Sidenav/Sidenav.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import { SideNav, SideNavItem, Button, Icon } from 'react-materialize'
+import { SideNav, SideNavItem, Button, Icon, Collapsible, CollapsibleItem } from 'react-materialize'
 import CircleButton from '../CircleButton'
 
 class Sidenav extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            closeSidenav: true
+            iconCollapsibleItem: ['chevron_right','folder']
         }
+    }
+
+    handlerFolder = () => {
+        this.setState(prevState => {
+            if(prevState.iconCollapsibleItem[1] === 'folder'){
+                return {
+                    iconCollapsibleItem: ['expand_more','folder_open']
+                }
+            }
+
+            return {
+                iconCollapsibleItem: ['chevron_right','folder']
+            }
+        })
     }
 
     render() {
         return (
             <SideNav
-                className='side-nav grey lighten-1'
+                className='side-nav grey lighten-2'
                 trigger={
                     <Button
                         floating large
@@ -22,24 +36,32 @@ class Sidenav extends Component {
                         <Icon className={this.props.style.IconClassName}> {this.props.style.icon}</Icon>
                     </Button>
                 }
-                options={{ closeOnClick: this.state.closeSidenav }}>
+                options={{ closeOnClick: true }}>
 
-                <section class='content-sidenav'>
+                <section className='content-sidenav'>
                     <header>
-                        {/* <Button
-                            floating large
-                            className={this.props.style.ButtonClassName}
-                            waves='light'
-                            onClick={() => this.setState({closeSidenav:true})}>
-                            <Icon className={this.props.style.IconClassName}> delete </Icon>
-                        </Button> */}
+                        <SideNavItem className='delete-sidenav'>
+                            <Icon className={this.props.style.IconClassName}>close</Icon>
+                        </SideNavItem>
                         <h4>VG</h4>
                         <h5>Welcome</h5>
                     </header>
-                    <SideNavItem href='#!second'>Second Link</SideNavItem>
+                    <Collapsible>
+                        <CollapsibleItem header='new-folder' icon={this.state.iconCollapsibleItem} onClick={this.handlerFolder}>
+                            Lorem ipsum dolor sit amet.
+                        </CollapsibleItem>
+                        <CollapsibleItem header='Second' icon='folder'>
+                            Lorem ipsum dolor sit amet.
+                        </CollapsibleItem>
+                        <CollapsibleItem header='Third' icon='folder'>
+                            Lorem ipsum dolor sit amet.
+                        </CollapsibleItem>
+                    </Collapsible>
+                    <CircleButton ButtonClassName='grey' IconClassName='black-font' icon='add'/>
+                    {/* <SideNavItem href='#!second'>Second Link</SideNavItem>
                     <SideNavItem divider />
                     <SideNavItem subheader>Subheader</SideNavItem>
-                    <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
+                    <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem> */}
                 </section>
 
             </SideNav>
