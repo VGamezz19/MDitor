f2e831d6fecbf62a8eba70a6a7f7c882dd8de3fd
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-16 09:45:39 +0100

    CicleButton test.fixture

--- src/components/CircleButton/_circleButton.test.fixture.js
+++ src/components/CircleButton/_circleButton.test.fixture.js
@@ -1,13 +1,44 @@
 import CircleButton from './';
-// const { className, iconClassName, icon, onClick  } = props
 
-export default {
-  component: CircleButton,
-  name: "testCosmos with Materialize Styles",
-  props: {
-    className: 'Lorem ipsum',
-    iconClassName: 'some class',
-    icon: 'delete',
-    onClick: () => console.log("Hello Cosmos!")
-  }
-};
+export default [{
+    component: CircleButton,
+    name: "Edit mode",
+    props: {
+      className: 'grey lighten-2',
+      iconClassName: 'black-font',
+      icon: 'edit',
+      onClick: () => console.log("Hello Cosmos!")
+    }
+  },
+  {
+    component: CircleButton,
+    name: "View mode",
+    props: {
+      className: 'grey lighten-2',
+      iconClassName: 'black-font',
+      icon: 'remove_red_eye',
+      onClick: () => console.log("Hello Cosmos!")
+    }
+  },
+  {
+    component: CircleButton,
+    name: "Add new File",
+    props: {
+      className: 'add-files grey lighten-3',
+      iconClassName: 'black-font',
+      icon: 'add',
+      onClick: () => console.log("Hello Cosmos!")
+    }
+  },
+  {
+    component: CircleButton,
+    name: "Add new Folder",
+    props: {
+      className: 'add-folder grey',
+      iconClassName: 'black-font',
+      icon: 'add',
+      onClick: () => console.log("Hello Cosmos!")
+    }
+  },
+];
+
