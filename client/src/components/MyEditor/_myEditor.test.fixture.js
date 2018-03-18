f3a4ead84aa9311f3c62a89ac441d83bc07d4a5f
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-18 09:22:12 +0100

    cosmos myEditor prototype File

--- client/src/components/MyEditor/_myEditor.test.fixture.js
+++ client/src/components/MyEditor/_myEditor.test.fixture.js
@@ -1,23 +1,27 @@
 import MyEditor from './'
+import File from 'mditor-types'
 
-export default [
-{
-    component: MyEditor,
-    name: 'MyEditor with out content',
-    url: '/',
-    props: {
-     file: {},
-     emitCurrentContent:(content) => console.log(`Content: ${content}`)
-    }
-},
+/**
+ * Mandatory data type FILE
+ */
+const file = new File("file", 3, "new File (1)", "#  It Should view like H1 !! \n ## H2 \n ### H3 \n ..")
+export default [{
+        component: MyEditor,
+        name: 'MyEditor with out content',
+        url: '/',
+        props: {
+            file: new File('file', 5, "new File(2)"),
+            emitCurrentContent: (content) => console.log(`Content: ${content}`)
+        }
+    },
 
-{
-    component: MyEditor,
-    name: 'MyEditor with some file',
-    url: '/',
-    props: {
-     file: { id: 1, title: 'new file (1)', content: '#  It Should view like H1 !! \n ## H2 \n ### H3 \n ...' },
-     emitCurrentContent:(content) => console.log(`Content: ${content}`) 
+    {
+        component: MyEditor,
+        name: 'MyEditor with some file',
+        url: '/',
+        props: {
+            file,
+            emitCurrentContent: (content) => console.log(`Content: ${content}`)
+        }
     }
-}
 ]
