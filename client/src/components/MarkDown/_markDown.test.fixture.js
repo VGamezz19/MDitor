05b7f50ae732f9d4942ad6ad6cd67d234cf0761c
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-18 09:25:06 +0100

    cosmos markDown prototype File

--- client/src/components/MarkDown/_markDown.test.fixture.js
+++ client/src/components/MarkDown/_markDown.test.fixture.js
@@ -1,31 +1,38 @@
 import MarkDown from './'
+import File from 'mditor-types'
+
+/**
+ * Mandatory data type FILE
+ */
+const file = new File("file", 3, "new File (1)", "#  It Should view like H1 !! \n ## H2 \n ### H3 \n ..")
+const fileEmpty = new File("file", 4, "new File(2)")
 
 export default [{
-    component: MarkDown,
-    name: 'HomaPage MardkDown',
-    url: '/',
-    props: {
-     file: {},
-     showInitialMarkDown:true   
-    }
-},
-{
-    component: MarkDown,
-    name: 'MardkDown with out content',
-    url: '/',
-    props: {
-     file: {},
-     showInitialMarkDown:false   
-    }
-},
+        component: MarkDown,
+        name: 'HomaPage MardkDown',
+        url: '/',
+        props: {
+            file: fileEmpty,
+            showInitialMarkDown: true
+        }
+    },
+    {
+        component: MarkDown,
+        name: 'MardkDown with out content',
+        url: '/',
+        props: {
+            file: fileEmpty,
+            showInitialMarkDown: false
+        }
+    },
 
-{
-    component: MarkDown,
-    name: 'MardkDown with some file',
-    url: '/',
-    props: {
-     file: { id: 1, title: 'new file (1)', content: '#  It Should view like H1 !! \n ## H2 \n ### H3 \n ...' },
-     showInitialMarkDown:false   
+    {
+        component: MarkDown,
+        name: 'MardkDown with some file',
+        url: '/',
+        props: {
+            file,
+            showInitialMarkDown: false
+        }
     }
-}
 ]
