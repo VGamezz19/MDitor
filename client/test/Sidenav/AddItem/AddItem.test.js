923218b272f74fb08a4539a882fdd928bc50aa7e
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-18 13:31:19 +0100

    JEST addItem

--- client/test/Sidenav/AddItem/AddItem.test.js
+++ client/test/Sidenav/AddItem/AddItem.test.js
@@ -1,57 +1,35 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import renderer from 'react-test-renderer';
-// import { mount } from 'enzyme';
-// import { MemoryRouter } from 'react-router';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 
-// import Sidenav from '../../src/components/Sidenav';
-// import API from '../../src/api/ApiClient';
-// import logicApp from '../../src/components/logicApp';
+import AddItem from '../../../src/components/Sidenav/AddItem';
 
-// const dataFolders = API.getFolders()
+it('Main aplication rendering', () => {
 
-// /**
-//  * Mandatory data type FILE
-//  */
-// const folders = logicApp.refactorDataToFileType(dataFolders)
+    const div = document.createElement('div');
 
-// const logicFolder = {
-//     create: () => console.log("Create Folder"),
-//     update: () => console.log("UpdateFolder"),
-//     delete: () => console.log("Delete Folder"),
-//     logicFile: {
-//         create: () => console.log("create File"),
-//         delete: () => console.log("delete File")
-//     }
-// }
-// const buttonTriggerStyle = { className: 'grey lighten-2', icon: 'menu' }
-// const buttonDropStyle = { className: 'grey lighten-2', icon: 'close' }
-// const user = { name: "Victor", surname: "Gamez" }
+    const wrapper = <AddItem
+        itemType='file'
+        inputType='text'
+        onSubmit={() => console.log("submit")}
+        onCancel={() => console.log("cancel")} />
 
-// it('Main aplication rendering', () => {
-//     const div = document.createElement('div');
+    ReactDOM.render(wrapper, div);
 
-//     const wrapper = <Sidenav
-//             folders={folders}
-//             logicFolder={logicFolder}
-//             buttonTriggerStyle={buttonTriggerStyle}
-//             buttonDropStyle={buttonDropStyle}
-//             user={user} />
+    ReactDOM.unmountComponentAtNode(div);
+});
 
-//     ReactDOM.render(wrapper, div);
-//     ReactDOM.unmountComponentAtNode(div);
-// });
+it('Should create and Match same Snapshot', () => {
 
-// it('Should create and Match same Snapshot', () => {
-//     const wrapper = <Sidenav
-//             folders={folders}
-//             logicFolder={logicFolder}
-//             buttonTriggerStyle={buttonTriggerStyle}
-//             buttonDropStyle={buttonDropStyle}
-//             user={user} />
+    const wrapper = <AddItem
+        itemType='file'
+        inputType='text'
+        onSubmit={() => console.log("submit")}
+        onCancel={() => console.log("cancel")} />
 
-//     const component = renderer.create(wrapper);
-//     let tree = component.toJSON();
+    const component = renderer.create(wrapper);
 
-//     expect(tree).toMatchSnapshot();
-// });
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
