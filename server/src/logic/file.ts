09f10c4d917319a426c07cc2483368a1c4a4d8ed
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-20 12:19:12 +0100

    done logic File

--- server/src/logic/file.ts
+++ server/src/logic/file.ts
@@ -11,7 +11,7 @@ const file = {
                 return Folder.findById({ _id: folderId });
             })
             .then(async (folder) => {
-                // console.log("creating-file ....", folder);
+
                 const file = new File({ title, content: "" });
 
                 folder.files.push(file._id);
@@ -23,13 +23,52 @@ const file = {
             });
     },
 
-    update: function updateFile(_id: mongoose.Types.ObjectId, title: string): Promise<mongoose.Types.ObjectId> | never {
+    update: function updateFile(_id: mongoose.Types.ObjectId, title?: string, content?: string): Promise<mongoose.Types.ObjectId> | never {
         return Promise.resolve()
             .then(() => {
-                validate({ _id, title });
-                return File.findOneAndUpdate({ _id }, { title });
+                validate({ _id });
+
+                const twoParameter = title && content;
+
+                if (twoParameter) {
+                    return File.findOneAndUpdate({ _id }, { title, content });
+                }
+
+                if (title) {
+                    return File.findOneAndUpdate({ _id }, { title });
+                }
+
+                if (content) {
+                    return File.findOneAndUpdate({ _id }, { content });
+                }
+
+                throw new Error("file.update need something to do Update [update(_id: ObjectId, title?: string, content?: string)]");
             })
             .then(file => file._id);
+    },
+
+    retrieve: function retrieveFile(_id: mongoose.Types.ObjectId) {
+        return Promise.resolve()
+            .then(() => {
+                validate({ _id });
+                return File.findById({ _id });
+            });
+    },
+
+    list: function listFile() {
+        return File.find();
+    },
+
+    delete: function deleteFile(folderId: mongoose.Types.ObjectId, _id: mongoose.Types.ObjectId) {
+        return Promise.resolve()
+            .then(() => {
+                validate({ _id });
+                return File.remove({ _id });
+            })
+            .then(() => {
+                return Folder.update({ _id: folderId }, { $pull: { files: _id } });
+            })
+            .then(() => _id);
     }
 };
 
