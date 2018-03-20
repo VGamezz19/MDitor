22bf22c6131a416cf53a663deced51f5136b4e6b
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-20 11:31:33 +0100

    fix-strong logic Validate

--- server/src/logic/validate.ts
+++ server/src/logic/validate.ts
@@ -4,16 +4,21 @@ import { IFile, IFolder } from "../Models";
  * validate File/Folder;
  * @param item
  */
-function validate(item: any): void | never {
+function validate(item: any): boolean | never {
+
+    const beAnObject = item instanceof Object && !(item instanceof Array) && !(item instanceof Function);
+
+    if (!beAnObject || item instanceof Array) throw Error(`validate(item: Object{}) only use Objects and you entered ${item}`);
+
     for (const prop in item) {
         const value = item[prop];
 
         if (typeof value === "undefined") throw Error(`${prop} cannot be undefined or empty`);
 
-        if (typeof value === "string")  if (!value.trim().length) throw Error(`${prop} cannot be undefined or empty`);
+        if (typeof value === "string") if (!value.trim().length) throw Error(`${prop} cannot be undefined or empty`);
 
     }
-    return;
+    return true;
 }
 
 export { validate };
--- server/test/logicValidate.test.ts
+++ server/test/logicValidate.test.ts
@@ -1,6 +1,21 @@
 import { validate } from "../src/logic";
 import "jest";
 
+
+// function validate(item: any): void | never {
+//     for (const prop in item) {
+//         const value = item[prop];
+
+//         if (typeof value === "undefined") throw Error(`${prop} cannot be undefined or empty`);
+
+//         if (typeof value === "string")  if (!value.trim().length) throw Error(`${prop} cannot be undefined or empty`);
+
+//     }
+//     return;
+// }
+
+// export { validate };
+
 describe("function Validate", () => {
 
     test("Should exist", () => {
@@ -8,5 +23,37 @@ describe("function Validate", () => {
         expect(validate);
 
         expect(validate).toBeInstanceOf(Function);
+    });
+
+    test("Should throw an Error", () => {
+
+        const valueUndefined = undefined;
+
+        const valueString = "";
+
+        const valueStringSpaces = "     ";
+
+        // tslint:disable-next-line:no-null-keyword
+        const valueNull = null;
+
+        const valueEmptyArray = [];
+
+        const valueEmptyObject = {};
+
+        const valueFunction = function hello() { };
+
+        expect(function () { validate(valueUndefined); }).toThrow("validate(item: Object{}) only use Objects");
+
+        expect(function () { validate(valueString); }).toThrow("validate(item: Object{}) only use Objects");
+
+        expect(function () { validate(valueStringSpaces); }).toThrow("validate(item: Object{}) only use Objects");
+
+        expect(function () { validate(valueNull); }).toThrow("validate(item: Object{}) only use Objects");
+
+        expect(function () { validate(valueEmptyArray); }).toThrow("validate(item: Object{}) only use Objects");
+
+        expect(function () { validate(valueEmptyObject); }).not.toThrow("validate(item: Object{}) only use Objects");
+
+        expect(function () { validate(valueFunction); }).toThrow("validate(item: Object{}) only use Objects");
     });
 });
