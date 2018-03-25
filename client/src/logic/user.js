96e12586ee6419f7b7f68a4965cc2fc78a6c339f
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-25 17:49:30 +0200

    JSDoc logic/user

--- client/src/logic/user.js
+++ client/src/logic/user.js
@@ -1,25 +1,62 @@
+/**
+ * Business UserApi, from mditor-api module (../api/ project)
+ */
 import { UserApi } from "mditor-api";
 
 const userApilogic = new UserApi("http", "localhost", "5050");
 
 export let tokenUser;
 
+/**
+ * user logic client side (bussines manager)
+ *
+ * Defined a logic to manager User session in client side.
+ *
+ * @version 1.0.0
+ */
 const userLogic = {
 
-    login: async (user, pass) => {
+    /**
+     * 
+     * Logic User function login
+     *
+     * function to verify user credentials
+     * 
+     * @param {String} user user name
+     * @param {String} pass password user
+     *
+     * @returns {Promise<JWT>} JWT with user information (_id)
+     *
+     * @version 1.0.0
+     */
+    login: (user, pass) => {
 
-        tokenUser = await userApilogic.login(user, pass)
-            .then(res => res.data.token);
+        return userApilogic.login(user,pass)
+            .then(res => {
+                return tokenUser = res.data.token
+            })
+        // tokenUser = await userApilogic.login(user, pass)
+        //     .then(res => res.data.token);
 
-        return tokenUser
+        // return tokenUser
     },
 
-    retrieve: async (token) => {
+    /**
+     * 
+     * Logic User function retrieve
+     *
+     * function to retrieve all information about this user
+     * 
+     * @param {JWT} token mandatory JWT from this user
+     *
+     * @returns {Promise<Object>} Object with User information, folders and files
+     *
+     * @version 1.0.0
+     */
+    retrieve: (token) => { 
 
-        const userData = await userApilogic.retrieve(token)
-            .then(res => res.data);;
-
-        return userData
+        return userApilogic.retrieve(token)
+            .then(res => res.data);
     }
 }
 
