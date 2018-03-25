1c3bbd32e29e3a6c36c00f1f2da0d016031e6dcb
VGamezz19 <victor.gamez1596@gmail.com>
2018-03-25 19:02:39 +0200

    JSDoc File.ts

--- mditor-types/src/File.ts
+++ mditor-types/src/File.ts
@@ -1,3 +1,10 @@
+/**
+ *
+ * DataType File, composite pattern
+ * https://en.wikipedia.org/wiki/Composite_pattern
+ *
+ * @version 1.0.0
+ */
 class File {
     private id: string;
     private type: "folder" | "file";
@@ -27,22 +34,126 @@ class File {
         this.type = _type;
     }
 
+    /**
+     *
+     * method getId
+     *
+     * Compatibility with File and Folder Type
+     *
+     * @returns {String} get this.id
+     *
+     * @version 1.0.0
+     */
     getId(): string { return this.id; }
 
+    /**
+     *
+     * method getTitle
+     *
+     * Compatibility with File and Folder Type
+     *
+     * @returns {String} get this.title
+     *
+     * @version 1.0.0
+     */
     getTitle(): string { return this.title; }
 
+    /**
+     *
+     * method getType
+     *
+     * Compatibility with File and Folder Type
+     *
+     * @returns {String} get this.type
+     *
+     * @version 1.0.0
+     */
     getType(): string { return this.type; }
 
+    /**
+     *
+     * method getContent
+     *
+     * Compatibility only with File Type
+     *
+     * @returns {String} get this.content
+     *
+     * @throws {Error} - mandatory Type File
+     *
+     * @version 1.0.0
+     */
     getContent(): string | never { return this.type === "file" ? this.content : this.throwError("is not a function", this.getContent.name); }
 
+    /**
+     *
+     * method getFile
+     *
+     * Compatibility only with Folder Type
+     *
+     * @param {String} id - id from retrieved file
+     *
+     * @returns {File} get retrieve file
+     *
+     * @throws {Error} - mandatory Type Folder
+     *
+     * @version 1.0.0
+     */
     getFile(id: string): File | never { return this.type === "folder" ? this.files.find(file => file.id === id) : this.throwError("is not a function", this.getFile.name); }
 
+    /**
+     *
+     * method setTitle
+     *
+     * Compatibility with Folder and File Type
+     *
+     * @param {String} title - set title
+     *
+     * @version 1.0.0
+     */
     setTitle(title: string) { this.title = title; }
 
+    /**
+     *
+     * method getFile
+     *
+     * Compatibility only with File Type
+     *
+     * @param {String} id - id from retrieved file
+     *
+     * @returns {File} get retrieve file
+     *
+     * @throws {Error} - mandatory Type File
+     *
+     * @version 1.0.0
+     */
     setContent(content: string) { this.type === "file" ? this.content = content : this.throwError("is not a function", this.setContent.name); }
 
+    /**
+     *
+     * method add
+     *
+     * Compatibility only with Folder Type
+     *
+     * @param {File} childr  children file type file to add
+     *
+     * @throws {Error} - mandatory Type Folder
+     *
+     * @version 1.0.0
+     */
     add(childr: File) { this.type === "folder" ? this.files.push(childr) : this.throwError("is not a function", this.add.name); }
 
+    /**
+     *
+     * method remove
+     *
+     * Compatibility only with Folder Type
+     *
+     * @param {File} child  children file type file to remove
+     *
+     * @throws {Error} - mandatory Type Folder
+     *
+     * @version 1.0.0
+     */
     remove(child: File) {
         if (this.type === "folder") {
             const length = this.files.length;
@@ -59,6 +170,13 @@ class File {
 
     }
 
+    /**
+     *
+     * method throwError
+     *
+     * @param {String} message descriptive message about error
+     * @param {String} type type error or name error
+     */
     throwError(message: string, type: string): never { throw new Error("Error: " + type + " " + message); }
 }
 
